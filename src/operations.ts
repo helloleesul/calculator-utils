d8177e99d1c96933911f8fca05a080b007bea40a
helloleesul <suerish.e@gmail.com>
2024-08-26 11:36:37 +0900

    :sparkles: error 함수 반영

--- src/operations.ts
+++ src/operations.ts
@@ -1,3 +1,5 @@
+import {getErrorMessage} from "./error.js";
+
 // 덧셈
 export const add = (a: number, b: number) => a + b;
 // 뺄셈
@@ -7,7 +9,7 @@ export const multiply = (a: number, b: number) => a * b;
 // 나눗셈
 export const divide = (a: number, b: number) => {
   if (b === 0) {
-    throw new Error("나눌 수 없는 숫자입니다.");
+   getErrorMessage('divisionByZero')
   }
   return a / b;
 };
